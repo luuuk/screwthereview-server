9c6d688a141b9fbb12d97a1fdc7316dba0a33963
Luke LeVasseur <lukelev@uw.edu>
2021-05-09 18:33:23 -0700

    Heroku deployment

---
+++ __tests__/server.test.js
@@ -0,0 +1,14 @@
+// const request = require('supertest')
+// const app = 'https://hidden-mesa-04199.herokuapp.com/'
+// const app_local = require('../src/server.js')
+
+// var yelp = require('supertest')
+// yelp = yelp('https://api.yelp.com/v3/businesses/search')
+
+// describe('Yelp ', () => {
+//   it('ensures that the Yelp API is functional', async () => {
+//         const response = await yelp.get('').set('location', 'austin').set('Authorization', 'Bearer ' + process.env.YELP_API_KEY)
+//         console.log(response)
+//         expect(response).to
+//     });
+// })
