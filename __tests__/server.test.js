ebc7293daee2112afc401b3170fe4e159ab11e84
Luke LeVasseur <lukelev@uw.edu>
2021-05-10 19:53:58 -0700

    refactored server tests into remote and local

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,24 +1,14 @@
-var request = require('supertest')
-const server_url = 'https://hidden-mesa-04199.herokuapp.com/'
-request = request(server_url)
+/* eslint-disable no-undef */
+/* eslint-disable indent */
+let request = require('supertest');
+
+const URL = 'https://hidden-mesa-04199.herokuapp.com/';
+request = request(URL);
 
 describe('Server tests ', () => {
-    it('ensures that the heroku deployed server is functional', async () => {
-          const response = await request.get('').set('location', 'austin')
-          var result = JSON.parse(response.text)
-          expect(result.id).toHaveLength(22)
-      });
-  })
-
-var yelp = require('supertest')
-yelp = yelp('https://api.yelp.com/v3/businesses/search')
-
-// describe('Yelp ', () => {
-//   it('ensures that the Yelp API is functional', async () => {
-//         const response = await yelp.get('').set('location', 'austin').set('Authorization', 'Bearer ' + process.env.YELP_API_KEY)
-//         console.log(response)
-//         expect(response).to
-//     });
-// })
-
-const app_local = require('../src/server.js')
+  it('ensures that the heroku deployed server is functional', async () => {
+      const response = await request.get('').set('location', 'austin');
+      const result = JSON.parse(response.text);
+      expect(result.id).toHaveLength(22);
+  });
+});
