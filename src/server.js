cd2ffd5738ae013214f71c8e0acc8aa28734b372
Luke LeVasseur <lukelev@uw.edu>
2021-05-11 13:10:35 -0700

    disabled console log error

--- src/server.js
+++ src/server.js
@@ -3,5 +3,6 @@ const app = require('./app');
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`App listening on port ${port}`);
 });
